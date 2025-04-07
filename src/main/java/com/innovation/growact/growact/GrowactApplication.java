7ed5c548f46075ed7724d72ca1e3372115a48c2f
Lucas-Viesan <vieiratm01@hotmail.com>
2025-04-07 18:04:40 -0300

    Subindo projeto com todas as configuracoes de banco e dependencias realizadas

---
+++ src/main/java/com/innovation/growact/growact/GrowactApplication.java
@@ -0,0 +1,13 @@
+package com.innovation.growact.growact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowactApplication.class, args);
+	}
+
+}
